3da8b7bedeb109c99008a8eeaba54ed32514a87b
Mojens <moha135h@stud.kea.dk>
2022-08-30 14:42:56 +0200

    commit

--- src/main/java/dat3/cars/api/MemberController.java
+++ src/main/java/dat3/cars/api/MemberController.java
@@ -1,23 +1,60 @@
 package dat3.cars.api;
-
-import dat3.cars.entity.Member;
-import dat3.cars.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dat3.cars.dto.MemberRequest;
+import dat3.cars.dto.MemberResponse;
+import dat3.cars.service.MemberService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/members")
 public class MemberController {
-  @Autowired  //Deliberately added via Autowired, remove this endpoint when you know why it's bad
-  MemberRepository memberRepository;
-  @GetMapping("/bad")
-  public List<Member> getMembersBad(){
-    return memberRepository.findAll();
+
+  MemberService memberService;
+
+  public MemberController(MemberService memberService){
+    this.memberService = memberService;
   }
+
+  //Security ADMIN
+  @GetMapping
+  List<MemberResponse> getMembers(){
+    return memberService.findMembers();
+  }
+
+  //Security ADMIN
+  @GetMapping(path = "/{username}")
+  MemberResponse getMemberById(@PathVariable String username) throws Exception {return null;}
+
+  //Security --> USER
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  //@PostMapping // same as above when you are using @RestController
+  MemberResponse addMember(@RequestBody MemberRequest body){
+    return null;
+  }
+
+  //Just to demonstrate that clients can control type of what they get back
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+  String addMember2(@RequestBody MemberRequest body){
+    return null;
+  }
+
+  //Security USER / ADMIN ???
+  @PutMapping("/{username}")
+  ResponseEntity<Boolean> editMember(@RequestBody MemberRequest body, @PathVariable String username){
+    return null;
+  }
+
+  //Security ADMIN
+  @PatchMapping("/ranking/{username}/{value}")
+  void setRankingForUser(@PathVariable String username, @PathVariable int value) {}
+
+  // Security ADMIN / USER
+  @DeleteMapping("/{username}")
+  void deleteMemberByUsername(@PathVariable String username) {}
+
 
 
 }
--- src/main/java/dat3/cars/service/MemberService.java
+++ src/main/java/dat3/cars/service/MemberService.java
@@ -1,4 +1,22 @@
 package dat3.cars.service;
 
+import dat3.cars.dto.MemberResponse;
+import dat3.cars.entity.Member;
+import dat3.cars.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+  MemberRepository memberRepository;
+  public MemberService(MemberRepository memberRepository){
+    this.memberRepository = memberRepository;
+  }
+
+  public List<MemberResponse> findMembers() {
+    List<Member> members = memberRepository.findAll();
+    List<MemberResponse> response = members.stream().map(member -> new MemberResponse(member,false)).toList();
+    return response;
+  }
 }
