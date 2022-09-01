1a3ce9f5b302a0ae692ca3a0fa704d43745d21a5
Mojens <moha135h@stud.kea.dk>
2022-09-01 14:24:23 +0200

    carRep

--- src/main/java/dat3/cars/service/CarService.java
+++ src/main/java/dat3/cars/service/CarService.java
@@ -1,4 +1,28 @@
 package dat3.cars.service;
 
+import dat3.cars.dto.CarResponse;
+import dat3.cars.dto.MemberResponse;
+import dat3.cars.entity.Car;
+import dat3.cars.repository.CarRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+  CarRepository carRepository;
+
+  public CarService(CarRepository carRepository){
+    this.carRepository = carRepository;
+  }
+
+  public List<CarResponse> findCars(){
+    List<Car> cars = carRepository.findAll();
+    List<CarResponse> carResponses = cars.stream().map(car -> new CarResponse(car,false)).toList();
+    return carResponses;
+  }
+  
+
+
 }
