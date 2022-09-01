2e5d43f0453ba315e753fc804ba14e859fe160e4
Mojens <moha135h@stud.kea.dk>
2022-09-01 15:26:53 +0200

    car controller

--- src/main/java/dat3/cars/api/CarController.java
+++ src/main/java/dat3/cars/api/CarController.java
@@ -1,11 +1,14 @@
 package dat3.cars.api;
 
+import dat3.cars.dto.CarRequest;
 import dat3.cars.dto.CarResponse;
+import dat3.cars.dto.MemberRequest;
+import dat3.cars.dto.MemberResponse;
 import dat3.cars.service.CarService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,7 +34,31 @@ public class CarController {
     return carService.findCarByID(id);
   }
 
-  
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  CarResponse addMember(@RequestBody CarRequest body){
+    return carService.addCar(body);
+  }
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.TEXT_HTML_VALUE)
+  public String addMember2(@RequestBody CarRequest body) {
+    carService.addCar(body);
+    return  body.getId() + " was created";
+  }
+
+  @PutMapping("/{id}")
+  ResponseEntity<Boolean> editMember(@RequestBody CarRequest body, @PathVariable int id){
+    carService.editCar(body,id);
+    return new ResponseEntity<>(true, HttpStatus.OK);
+  }
+  @PatchMapping("/bestDiscount/{id}/{value}")
+  void setBestDiscountForCar(@PathVariable int id, @PathVariable double value){
+    carService.setBestDiscountForCar(id,value);
+  }
+
+  @DeleteMapping("/{id}")
+  void deleteMemberByUsername(@PathVariable int id) {
+    carService.deleteByid(id);
+  }
 
 
 
